cd61b70ab7c789c47c94712af8d969d8a342aebc
BorelNathan <nathan.champommier-borel@etu.univ-amu.fr>
2022-01-12 13:52:01 +0100

    amelioration de gestionUser.php

--- admindataprocessing.php
+++ admindataprocessing.php
@@ -6,53 +6,128 @@
   $newlogin = $_POST['newloginUser'];
   $filter = filterUsers(4);
 
+  if($newlogin != ''){
+    $changementlogin = 'newloginUser';
+  }
+  else{
+    $changementlogin = '';
+  }
+
+
 
   if($filter == 1){
 
     start_page('adminprocess');
+      $i = $newUserID . $changementlogin;
+      echo $i ;
+      switch($i){
 
+        case "1":
+        case "2":
+        case "3":
+        case "4":
 
-
-      $dbLink = mysqli_connect('mysql-e-eventio.alwaysdata.net', 'e-eventio_login', 'php123456$') or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-      mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
-
-      $query = "SELECT * FROM users WHERE login =  '" . $login  . "'";
-
-
-      if(!($dbResult = mysqli_query($dbLink, $query)))
-         {
-           echo 'Erreur de requête<br/>';
-           echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-           echo 'Requête : ' . $query . '<br/>';
-          exit();
-        }
-      elseif ($dbRow = mysqli_fetch_row($dbResult))
-        {
           $dbLink = mysqli_connect('mysql-e-eventio.alwaysdata.net', 'e-eventio_login', 'php123456$') or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
           mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
-          $query2 = "UPDATE users SET id_role = '" . $newUserID . "' where login = '" . $login . "'";
-          mysqli_query($dbLink, $query2);
-          $_SESSION['UserChangeRoleID'] = 'Action enregistrer';
-          header('Location: gestionUsers.php');
-        }
-      else
-        {
-          $_SESSION['UserChangeRoleID'] = 'User non existant dans la base';
-          header('Location: gestionUsers.php');
-        }
+
+          $query = "SELECT * FROM users WHERE login =  '" . $login  . "'";
 
 
+          if(!($dbResult = mysqli_query($dbLink, $query)))
+             {
+               echo 'Erreur de requête<br/>';
+               echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+               echo 'Requête : ' . $query . '<br/>';
+              exit();
+            }
+          elseif ($dbRow = mysqli_fetch_row($dbResult))
+            {
+              $dbLink = mysqli_connect('mysql-e-eventio.alwaysdata.net', 'e-eventio_login', 'php123456$') or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+              mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+              $query2 = "UPDATE users SET id_role = '" . $newUserID . "' where login = '" . $login . "'";
+              mysqli_query($dbLink, $query2);
+              $_SESSION['UserChange'] = 'Action enregistrer';
+              header('Location: gestionUsers.php');
+            }
+          else
+            {
+              $_SESSION['UserChange'] = 'User non existant dans la base';
+              header('Location: gestionUsers.php');
+            }
+          break;
+
+        case "newloginUser":
+          $dbLink = mysqli_connect('mysql-e-eventio.alwaysdata.net', 'e-eventio_login', 'php123456$') or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+          mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
+          $query = "SELECT * FROM users WHERE login =  '" . $login  . "'";
+
+          if(!($dbResult = mysqli_query($dbLink, $query)))
+             {
+               echo 'Erreur de requête<br/>';
+               echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+               echo 'Requête : ' . $query . '<br/>';
+              exit();
+            }
+          elseif ($dbRow = mysqli_fetch_row($dbResult))
+            {
+              $dbLink = mysqli_connect('mysql-e-eventio.alwaysdata.net', 'e-eventio_login', 'php123456$') or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+              mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+              $query2 = "UPDATE users SET login = '" . $newlogin . "' where login = '" . $login . "'";
+              mysqli_query($dbLink, $query2);
+              $_SESSION['UserChange'] = 'Action enregistrer';
+              header('Location: gestionUsers.php');
+            }
+          else
+            {
+              $_SESSION['UserChange'] = 'User non existant dans la base';
+              header('Location: gestionUsers.php');
+            }
 
 
+          break;
+
+        case "1newloginUser":
+        case "2newloginUser":
+        case "3newloginUser":
+        case "4newloginUser":
+
+          $dbLink = mysqli_connect('mysql-e-eventio.alwaysdata.net', 'e-eventio_login', 'php123456$') or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+          mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+
+          $query = "SELECT * FROM users WHERE login =  '" . $login  . "'";
 
 
+          if(!($dbResult = mysqli_query($dbLink, $query)))
+             {
+               echo 'Erreur de requête<br/>';
+               echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+               echo 'Requête : ' . $query . '<br/>';
+              exit();
+            }
+          elseif ($dbRow = mysqli_fetch_row($dbResult))
+            {
+              $dbLink = mysqli_connect('mysql-e-eventio.alwaysdata.net', 'e-eventio_login', 'php123456$') or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+              mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+              $query2 = "UPDATE users SET id_role = '" . $newUserID . "' where login = '" . $login . "'";
+              $query3 = "UPDATE users SET login = '" . $newlogin . "' where login = '" . $login . "'";
+              mysqli_query($dbLink, $query2);
+              mysqli_query($dbLink, $query3);
+              $_SESSION['UserChange'] = 'Action enregistrer';
+              header('Location: gestionUsers.php');
+            }
+          else
+            {
+              $_SESSION['UserChange'] = 'User non existant dans la base';
+              header('Location: gestionUsers.php');
+            }
 
+        break;
 
+        default:
+          echo 'chiasse';
 
-
-
-
-
+          }
 
     end_page();
 
--- gestionUsers.php
+++ gestionUsers.php
@@ -12,13 +12,14 @@
   <form action="admindataprocessing.php" method="POST" name="traitementrequetesadmin">
     Login de l'utilisateur : <input type="text" name="loginUser" required="required"/><br/>
     role_id a appliquer  : <select name="newRoleID"/><br/>
+      <option value="">Pas de modification</option>
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
       <option value="4">4</option>
     </select> </br>
-    Nouveau login pour l'utilisateur <input type="text" name="newloginUser"/><br/>  
-    <?php echo $_SESSION['UserChangeRoleID'] . '</br>'; ?>
+    Nouveau login pour l'utilisateur <input type="text" name="newloginUser"/><br/>
+    <?php echo $_SESSION['UserChange'] . '</br>'; ?>
     <input type="submit" name="action" value="Valider"/> <br/>
   </form>
   <a href="index.php">Retour</a>
