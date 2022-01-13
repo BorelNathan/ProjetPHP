95762a7c1182c2f4591f6946b2e536b8ba9e4c95
Maxime <maxime.monbeig@etu.univ-amu.fr>
2022-01-13 17:41:48 +0100

    Modif de Campagne nouveaux attributs + problèmes de date

--- AddCampagne.php
+++ AddCampagne.php
@@ -2,14 +2,21 @@
     require 'utils.inc.php';
     start_page('Créer une campagne');
     session_start();
-    $title = $_POST['EventTitle'];
+    $title = $_POST['CampTitle'];
     $date_deb = date_create_from_format('Y-m-d', $_POST['CampDateDeb']);
     $date_deb = $date_deb->format('Y-m-d');
     $date_fin = date_create_from_format('Y-m-d', $_POST['CampDateFin']);
     $date_fin = $date_fin->format('Y-m-d');
 
-    $points = $_POST['CampPoints'];
-    //$date = date();
+    if ($date_deb > $date_fin){
+        $_SESSION['MsgCampagne'] = 'Tu as mal choisi les dates de ta campagne !';
+        header('Location: CreateCampagne.php');
+        exit();
+    }
+
+    $points_attribués = $_POST['CampPointsDonnés'];
+    $points_requis = $_POST['CampPointsRequis'];
+    $description = $_POST['CampDescription'];
     $can_create = true;
     $user_id = $_SESSION['CurrentUserID'];
     $user_id_role = $_SESSION['CurrentUserIDRole'];
@@ -18,7 +25,8 @@
     mysqli_select_db($dbLink , 'e-eventio_sql') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
     $query_checkid = "SELECT * FROM campagne";
 
-    $query_insertcamp = "INSERT INTO campagne (date_deb, date_fin, point_attribué) VALUES ('$date_deb' " . ',' . " '$date_fin' " . ',' . " '$points')";
+    $query_insertcamp = "INSERT INTO campagne (date_deb, date_fin, point_attribué, point_min, description, titre) VALUES 
+                        ('" . $date_deb . "' , '" .  $date_fin . "' , " .  $points_attribués . "," . $points_requis . ",'" . $description . "','" . $title . "')";
     if(!($dbResult = mysqli_query($dbLink, $query_checkid)))
     {
         echo $_SESSION;
@@ -27,40 +35,40 @@
         echo 'Requête : ' . $query_checkid . '<br/>';
         exit();
     }
-    elseif ($dbRow = mysqli_fetch_row($dbResult))
+    else
     {
-        if ($dbRow[1] <= $date_deb && $dbRow[2] >= $date_deb) {
-            $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
-            header('Location: CreateCampagne.php');
-            $can_create = false;
-            exit();
-        }
-        elseif ($dbRow[1] <= $date_fin && $dbRow[2] >= $date_fin){
-            $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
-            header('Location: CreateCampagne.php');
-            $can_create = false;
-            exit();
-        }
-        elseif ($dbRow[1] >= $date_deb && $dbRow[1] <= $date_fin){
-            $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
-            header('Location: CreateCampagne.php');
-            $can_create = false;
-            exit();
-        }
-        elseif ($dbRow[2] >= $date_deb && $dbRow[2] <= $date_fin){
-            $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
-            header('Location: CreateCampagne.php');
-            $can_create = false;
-            exit();
-        }
-        elseif ($dbRow[1] == $date_deb && $dbRow[2] == $date_fin){
-            $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
-            header('Location: CreateCampagne.php');
-            $can_create = false;
-            exit();
-        }
-        else {
-            $can_create = true;
+        while ($dbRow = mysqli_fetch_row($dbResult))
+        {
+            if ($dbRow[1] <= $date_deb && $dbRow[2] >= $date_deb) {
+                $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
+                header('Location: CreateCampagne.php');
+                $can_create = false;
+                exit();
+            }
+            elseif ($dbRow[1] <= $date_fin && $dbRow[2] >= $date_fin){
+                $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
+                header('Location: CreateCampagne.php');
+                $can_create = false;
+                exit();
+            }
+            elseif ($dbRow[1] >= $date_deb && $dbRow[1] <= $date_fin){
+                $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
+                header('Location: CreateCampagne.php');
+                $can_create = false;
+                exit();
+            }
+            elseif ($dbRow[2] >= $date_deb && $dbRow[2] <= $date_fin){
+                $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
+                header('Location: CreateCampagne.php');
+                $can_create = false;
+                exit();
+            }
+            elseif ($dbRow[1] == $date_deb && $dbRow[2] == $date_fin){
+                $_SESSION['MsgCampagne'] = 'Il y a déjà une campagne en cours !';
+                header('Location: CreateCampagne.php');
+                $can_create = false;
+                exit();
+            }
         }
     }
 
@@ -74,7 +82,7 @@
             exit();
         }
         else {
-            $_SESSION['MsgCampagne'] = 'Ta campagne à été créée !';
+            $_SESSION['MsgCampagne'] = 'Ta campagne a été créée !';
             header('Location: CreateCampagne.php');
         }
     }
