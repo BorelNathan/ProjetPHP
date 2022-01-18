4eca352198c56bf7f686221b91122f3837ee4a6e
Pascal-Flores <pascal.flores@etu.univ-amu.fr>
2022-01-18 15:43:58 +0100

    oui ça marche la page d'admin

--- VraiProjet/View/AdminPage/ViewAdminPage.php
+++ VraiProjet/View/AdminPage/ViewAdminPage.php
@@ -1,9 +1,12 @@
 <?php $title='Mon espace perso'?>
 <?php ob_start(); ?>
 <?php if($filter == 1){?>
+<header>
+    	<img id="logo" src="../../resources/images/logo.svg" alt="logo de E-Event.IO" />
+</header>
 <main id="adminMain">
   <section>
-    <h1>PAGE RESERVE AUX ADMINS</h1>
+    <h1>PAGE RESERVEE AUX ADMINS</h1>
       <form action="/" method="POST" name="traitementrequetesadmin">
         <label for="loginUser">Login de l'utilisateur: </label>
         <input class="roundCornerLink" type="text" name="loginUser" required="required"/>
@@ -24,17 +27,25 @@
   <?php echo $_SESSION['UserChange'] ; ?>
         <input type="submit" name="action" value="ChangementUtilisateur"/> <br/>
       </form>
-      <form id="Retour" action="/" method="post">
+      <form id="home" action="/" method="post">
         <input type="hidden" name="action" value="Accueil"/>
       </form>
-      <a class="roundCornerLink"href='#' onclick='document.getElementById("Retour").submit()'>Retour à la page principale</a>
-      <form id="Retour" action="/" method="post">
-        <input type="hidden" name="action" value="Accueil"/>
+      <a class="roundCornerLink"href='#' onclick='document.getElementById("home").submit()'>Retour à la page principale</a>
+      <form id="myspace" action="/" method="post">
+        <input type="hidden" name="action" value="UserPage"/>
       </form>
-      <a class="roundCornerLink"href='#' onclick='document.getElementById("Retour").submit()'>Retour à la page principale</a>
+      <a class="roundCornerLink"href='#' onclick='document.getElementById("myspace").submit()'>Retour à mon espace</a>
   </section>
 </main>
-
+<footer>
+  <ul>
+    <li><a class="roundCornerLink" href="howitworks.php">Fonctionnement du site</a></li>
+    <li><div class="split"></div></li>
+    <li><a class="roundCornerLink" href="CGU.php">CGU</a></li>
+    <li><div class="split"></div></li>
+    <li><a class="roundCornerLink" href="legaldisclaimer.php">Mentions légales</a></li>
+  </ul>
+</footer>
 <?php } ?>
 <?php $content = ob_get_clean(); ?>
 
